380be90c507aeac320bab7e82d266e7c4dd24082
nolangh <nolanghiggins@gmail.com>
2023-11-11 15:17:47 -0700

    -a

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
+const MainContainer = styled.section`
+  background-color: coral;
+  height: 100vh;
+  width: 100%;
+`;
+
 function App() {
-  return <>TESt</>;
+  return (
+    <div>
+      <MainContainer>
+        <h1>TESt</h1>
+      </MainContainer>
+    </div>
+  );
 }
 
 export default App;
