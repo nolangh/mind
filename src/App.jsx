d2be42432b848a743d3bb822188d42a555b5543c
nolangh <nolanghiggins@gmail.com>
2023-11-15 14:06:36 -0700

    -a

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import "./styles/main.css";
 const App = () => {
   return (
     <>
-      <Container fluid className="mainContainer">
-        TEST
+      <Container fluid className="main_container">
+        main_container
+        <Container fluid classname="title_container">
+          title_container
+        </Container>
       </Container>
     </>
   );
