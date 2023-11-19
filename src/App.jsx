a5d18d5f7fe0393efb5b2c7940a49d5cd1753562
nolangh <nolanghiggins@gmail.com>
2023-11-19 12:59:37 -0700

    -a

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 import "./global.css";
+import "98.css";
+import Window from "./components/Window";
 
 const Main_container = styled.div`
 	background: coral;
 	width: 100vw;
+	height: 100vh;
 `;
 const Title_container = styled.div`
 	background: blue;
@@ -17,11 +20,11 @@ const Form_main_container = styled.div`
 const App = () => {
 	return (
 		<>
-			<Main_container>
+			<Window>
 				main_container
 				<Title_container>title_container</Title_container>
 				<Form_main_container>form_main_container</Form_main_container>
-			</Main_container>
+			</Window>
 		</>
 	);
 };
---
+++ src/components/Window.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "98.css";
+
+const Window = () => {
+	<>
+		<div className=" container-fluid window">Main Window</div>
+	</>;
+};
+
+export default Window;
