bbb58870966b0887a2a163c8573f0d7c075fed29
nolangh <nolanghiggins@gmail.com>
2023-11-19 12:32:56 -0700

    Got rid of the damn css errors in react by using styled-comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { Container } from "react-bootstrap";
+import styled from "styled-components";
 import "./global.css";
-//import "./styles/main.css";
+
+const Main_container = styled.div`
+	background: coral;
+	width: 100vw;
+`;
+const Title_container = styled.div`
+	background: blue;
+	width: 90%;
+`;
+const Form_main_container = styled.div`
+	background: lightgreen;
+	width: 80%;
+`;
 
 const App = () => {
 	return (
 		<>
-			<Container fluid className="main_container">
+			<Main_container>
 				main_container
-				<div className="title_container">title_container</div>
-				<Container fluid className="form_main_container">
-					form_main_container
-				</Container>
-			</Container>
+				<Title_container>title_container</Title_container>
+				<Form_main_container>form_main_container</Form_main_container>
+			</Main_container>
 		</>
 	);
 };
