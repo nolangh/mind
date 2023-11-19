ef4860d5ca5260d528c249aab3ada77a24522b58
nolangh <nolanghiggins@gmail.com>
2023-11-19 14:46:52 -0700

    -a

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import styled from "styled-components";
 import "./global.css";
 import "98.css";
 
-const Main_container = styled.div`
-	background: coral;
-	width: 50%;
-	height: 50%;
+const Container = styled.div`
+	display: flex;
+	background: darkgreen;
+	height: 100%;
+	width: 100%;
 `;
-const Title_container = styled.div`
+
+const Window = styled.div`
+	background: pink;
+	width: 50%;
+	margin: auto;
+`;
+
+const Title_bar = styled.div`
 	background: blue;
 	justify-content: center;
 `;
@@ -51,27 +59,29 @@ const App = () => {
 	return (
 		<>
 			{/**SECTION - Main Body Container */}
-			<Main_container fluid className="window">
-				{/*SECTION Title Container */}
-				<Title_container className="title-bar">
-					<Title className="title-bar-text">Mind Reader</Title>
-				</Title_container>
-				<Body_container>
-					<Left_image_container>svg container - Left</Left_image_container>
-					{/*SECTION Window Body */}
-					<Window_body className="window-body">
-						Window Body
-						{/*SECTION Input Container */}
-						<Input_Container className="field-row">
-							<Input_label>Enter your word</Input_label>
-							<input id="textInput" type="text" />
-						</Input_Container>
-						{/*SECTION Submit Button */}
-						<Button type="submit" />
-					</Window_body>
-					<Right_image_container>svg container - Right</Right_image_container>
-				</Body_container>
-			</Main_container>
+			<Container>
+				<Window className="window">
+					{/*SECTION Title Container */}
+					<Title_bar className="title-bar">
+						<Title className="title-bar-text">Mind Reader</Title>
+					</Title_bar>
+					<Body_container>
+						<Left_image_container>svg container - Left</Left_image_container>
+						{/*SECTION Window Body */}
+						<Window_body className="window-body">
+							Window Body
+							{/*SECTION Input Container */}
+							<Input_Container className="field-row">
+								<Input_label>Enter your word</Input_label>
+								<input id="textInput" type="text" />
+							</Input_Container>
+							{/*SECTION Submit Button */}
+							<Button type="submit" />
+						</Window_body>
+						<Right_image_container>svg container - Right</Right_image_container>
+					</Body_container>
+				</Window>
+			</Container>
 		</>
 	);
 };
