646c7c1ea9c2df68df4977474843bfb2aaacec3d
nolangh <nolanghiggins@gmail.com>
2023-11-19 12:18:59 -0700

    -a

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import "./global.scss";
 //import "./styles/main.scss";
 
 const App = () => {
-  return (
-    <>
-      <Container fluid className="main_container">
-        main_container
-        <div className="title_container">title_container</div>
-        <Container fluid className="form_main_container">
-          form_main_container
-        </Container>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Container fluid className="main_container">
+				main_container
+				<div className="title_container">title_container</div>
+				<Container fluid className="form_main_container">
+					form_main_container
+				</Container>
+			</Container>
+		</>
+	);
 };
 
 export default App;
